bb942234693ea3137bed023baef105be45190fd3
lunaru100 <lunaruteyvat@gmail.com>
2023-09-05 11:05:33 +0200

    Main holder add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    GenericHolder genericHolder = new GenericHolder(123);
+        System.out.println(genericHolder.getObj());
+        System.out.println(genericHolder.getObj().getClass().getSimpleName());
+        genericHolder.setObj(333);
+        System.out.println(genericHolder.getObj());
     }
 }
